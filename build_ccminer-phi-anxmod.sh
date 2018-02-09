90d73f44d7c3a8127bd0609b74e6e81870db6e9e
viatoro <jarupong.wiboonchart@gmail.com>
2018-02-09 12:51:23 +0700

    Update build_ccminer-phi-anxmod.sh

--- build_ccminer-phi-anxmod.sh
+++ build_ccminer-phi-anxmod.sh
@@ -3,7 +3,8 @@
 # Simple script to create the Makefile and build
 
 # export PATH="$PATH:/usr/local/cuda/bin/"
-git submodule foreach git pull
+#git submodule foreach git pull
 cd ccminer-phi-anxmod/ccminer/
+git pull
 sh build.sh
 cd -
