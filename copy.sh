8920a9d9bfa971c7228b25e16719453e536b7ff4
kwan <k@moomanow.com>
2018-05-03 13:26:50 +0700

    update

---
+++ copy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Simple script to create the Makefile and build
+
+# export PATH="$PATH:/usr/local/cuda/bin/"
+
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+cd ${1}/ccminer 
+make -j$(nproc)
+cp ccminer ../ccminer-run
+mkdir -p ~/miner/${1}
+cp ccminer ~/miner/${1}/ccminer-run
+mkdir -p ~/minerAutoSwitch/bin/${1}
+cp ccminer ~/minerAutoSwitch/bin/${1}/ccminer-run
+cd -
