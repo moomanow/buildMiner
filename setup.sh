98cf9a2001dda35cbf138b2a856b7961d9012c3b
viatoro <jarupong.wiboonchart@gmail.com>
2018-06-09 09:38:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,16 @@
 curl -o ~/.bash_aliases https://raw.githubusercontent.com/moomanow/buildMiner/master/.bash_aliases
 curl -o ~/.screenrc https://raw.githubusercontent.com/moomanow/buildMiner/master/.screenrc
 git clone https://github.com/moomanow/buildMiner.git
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install bc
+sudo apt-get -y install libcurl4-openssl-dev
+sudo apt-get -y install libjansson-dev
+sudo apt-get -y install automake
+cd buildMiner
+git submodule init
+git submodule foreach git reset --hard
+git submodule update
+git submodule foreach git pull
+
+sudo ln -s /usr/local/cuda-9.1 /usr/local/cuda
+sudo ln -s /usr/local/cuda-9.1 /usr/local/cuda-7.5
