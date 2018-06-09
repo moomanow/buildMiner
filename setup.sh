3c02700e2e4271cde79fa991f0931a912689202b
viatoro <jarupong.wiboonchart@gmail.com>
2018-06-09 09:32:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-
+#! /bin/sh
+curl -o ~/.bash_aliases https://raw.githubusercontent.com/moomanow/buildMiner/master/.bash_aliases
+curl -o ~/.screenrc https://raw.githubusercontent.com/moomanow/buildMiner/master/.screenrc
+git clone https://github.com/moomanow/buildMiner.git
